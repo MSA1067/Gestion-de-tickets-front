8fba5620f4a156c1b8ba08c8c2db2200e432b5f7
miguel.sierra <miguel.sierra@quipux.com>
2025-10-28 19:37:00 -0500

    Implementacion de nuevas pantallas funcionales (beta)

--- src/app/components/dashboard/ca/user/user.component.ts
+++ src/app/components/dashboard/ca/user/user.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuComponent} from "../../../../shared/menu/menu.component";
+import {RouterOutlet} from "@angular/router";
+import {SiderMenuComponent} from "../../../../shared/sider-menu/sider-menu.component";
+import {Subscription} from 'rxjs';
+import {MenuService} from '../../../../service/menu-service/menu.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    MenuComponent,
+    RouterOutlet,
+    SiderMenuComponent,
+    NgClass
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
 
+  isCollapsed = false;
+  private sub!: Subscription;
+
+  constructor(private menuService: MenuService,) {}
+
+  ngOnInit() {
+    this.sub = this.menuService.collapsed$.subscribe(value => {
+      this.isCollapsed = value;
+    });
+  }
 }
