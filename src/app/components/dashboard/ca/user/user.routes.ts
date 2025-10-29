f335caa0cbc5419de5bdc903aaadf7e993e24a8b
YeiderGarzon <yeidergarzon11@gmail.com>
2025-10-28 19:58:42 -0500

    Se añade componente de request

--- src/app/components/dashboard/ca/user/user.routes.ts
+++ src/app/components/dashboard/ca/user/user.routes.ts
@@ -1,11 +1,9 @@
 import {Routes} from '@angular/router';
-import {DashboardUserComponent} from './dashboard-user/dashboard-user.component';
 import {ProfileUserComponent} from './profile-user/profile-user.component';
-import {TicketsUserComponent} from './tickets-user/tickets-user.component';
+import { RequestsComponent } from './requests/requests.component';
 
 export const routes: Routes = [
-  {path: 'dashboard' , component: DashboardUserComponent},
   {path: 'profile' , component: ProfileUserComponent},
-  {path: 'tickets' , component: TicketsUserComponent},
+  {path: 'requests' , component: RequestsComponent},
   {path: '' , redirectTo: 'dashboard', pathMatch: 'full' },
 ]
